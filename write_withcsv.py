77de80eb6f1166d0d44e172036eafd7e2d786e45
Paurakh Saud <paurakhsaud2022@gmail.com>
2025-01-20 18:12:50 +0545

    (Learning) Write with csv file and generated some csv file to work with

---
+++ write_withcsv.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+dis = {"a": [1, 2, 3], "b": [4, 5, 6],"c":[7,8,9]}
+d = pd.DataFrame(dis)
+
+# Save the dataframe to a csv file
+
+d.to_csv("data2.csv", index=False, header=["x","y","z"]) # index=False is used to avoid writing row numbers in the csv file
+
